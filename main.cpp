470c0b636f47b0c209723fe7171bb7e2c670107d
Pawel C <c.pawel.10@gmail.com>
2023-11-24 00:00:36 -0700

    Implemented exception handling

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-
 struct BstNode{
   int data;
   BstNode* left;
@@ -9,17 +8,23 @@ struct BstNode{
 };
 
 
-
-class BST{
-  BstNode *root = NULL;
-public:
 BstNode* getNewNode(int data){
   BstNode* newNode = new BstNode();
   newNode->data = data;
   newNode->left = newNode->right = NULL;
   return newNode;
 }
-BstNode* insert(int data){
+
+// Function to perform in-order traversal
+void inOrderTraversal(BstNode* node) {
+    if (node != NULL) {
+        inOrderTraversal(node->left);   // Visit left subtree
+        std::cout << node->data << " "; // Visit node
+        inOrderTraversal(node->right);  // Visit right subtree
+    }
+}
+
+BstNode* insert(BstNode* root, int data){
   if(root==NULL){
     root = getNewNode(data);
 
@@ -31,18 +36,47 @@ BstNode* insert(int data){
   }
     return root;
 }
-};
 
+void remove(BstNode** root, const int &t){
+  if((*root)->data==t){
+    if((*root)->left==NULL && (*root)->left==NULL){
+      
+    }
+  }else{
+  try{
+    if((*root)->left==NULL && (*root)->left==NULL){
+      throw 1;
+    }
+  }
+  catch(int x){
+    cout<<"\nException thrown, BinarySearchTreeNodeNotFound exiting program";
+    exit(1);
+  }
 
+  if((*root)->data>=t){
+    BstNode** leftPtr = &((*root)->left);
+    remove(leftPtr,t);
 
-
-BstNode* remove(const int &t){
+  }else{
+    BstNode** rightPtr = &((*root)->right);
+    remove(rightPtr,t);
+  }
+  }
   
 }
 
 int main(){
   BstNode *root = NULL;
+  BstNode **rootPtr = &root;
   root=insert(root, 15);
-  root=insert(root, 10);
-  root=insert(root, 20);
+    root=insert(root, 12);
+      root=insert(root, 13);
+        root=insert(root, 14);
+          root=insert(root, 16);
+            root=insert(root, 17);
+              root=insert(root, 18);
+  inOrderTraversal(root);
+  remove(rootPtr,4);
+  cout<<"Finished";
+  return 0;
 }
